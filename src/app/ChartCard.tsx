ccaab0ea35298f8d22f3554bf8892adb1a04f118
Pawel Fajfer <fajfer.pawel@gmail.com>
2023-08-03 09:46:30 +0200

    feat: Added filtering MultiSelect

--- src/app/ChartCard.tsx
+++ src/app/ChartCard.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card"
 import {Chart} from "@/app/chart";
 import {PerformanceEntries} from "@/app/api/performanceEntriesApi";
+import {MultiSelect} from "@/components/ui/MultiSelect";
+import {isCached, isCrossOrigin} from "@/app/measuresMappers";
 
-export const ChartCard = ({performanceEntries}: {performanceEntries: PerformanceEntries}) => {
+export const ChartCard = ({performanceEntries}: { performanceEntries: PerformanceEntries }) => {
+
+    const [filters, setFilters] = useState<Array<'cache' | 'crossOrigin'>>(['cache' , 'crossOrigin'])
+    const chartData = performanceEntries.entries
+        .filter((entry) => filters.includes('crossOrigin') ? !isCrossOrigin(entry) : true)
+        .filter((entry) => filters.includes('cache') ? !isCached(entry) : true)
 
     return (
         <Card className={'w-full'}>
-            <CardHeader>
-                <CardTitle>Overview</CardTitle>
-                <CardDescription>Average resource fetching duration</CardDescription>
-            </CardHeader>
+            <div className={'flex justify-between'}>
+                <CardHeader>
+                    <CardTitle>Overview</CardTitle>
+                    <CardDescription>Average resource fetching duration</CardDescription>
+                </CardHeader>
+                <div className={'m-0 space-x-1.5 p-5 max-w-lg'}>
+                    <MultiSelect selected={filters} setSelected={setFilters} />
+                </div>
+            </div>
             <CardContent>
-                <Chart performanceEntries={performanceEntries} />
+                <Chart data={chartData}/>
             </CardContent>
         </Card>
     );
--- src/app/chart.tsx
+++ src/app/chart.tsx
@@ -2,32 +2,26 @@
 import React from 'react';
 import {Chart as ChartJS, registerables} from 'chart.js';
 import {Bar} from 'react-chartjs-2';
-import {Toggle} from "@/components/ui/toggle"
-import {useFilters} from "@/app/useFilters";
-import {PerformanceEntries} from "@/app/api/performanceEntriesApi";
+import {PerformanceEntry} from "@/app/api/performanceEntriesApi";
 
 ChartJS.register(...registerables);
 
-export const Chart = ({performanceEntries}: {performanceEntries: PerformanceEntries}) => {
-
-    const {filterData, chartData,filters} = useFilters(performanceEntries.entries);
+export const Chart = ({data}: {data: PerformanceEntry[]}) => {
 
     return (
         <div>
             <Bar data={
                 {
-                    labels: chartData.map((row) => row.shortName),
+                    labels: data.map((row) => row.shortName),
                     datasets: [
                         {
                             label: 'Request time in ms',
                             indexAxis: 'y',
-                            data: chartData.map((row) => row.duration)
+                            data: data.map((row) => row.duration)
                         }
                     ],
                 }
             }/>
-            <Toggle pressed={filters.cache} onPressedChange={(event)=>filterData(event, 'cache')}>Cached</Toggle>
-            <Toggle pressed={filters.crossOrigin} onPressedChange={(event)=>filterData(event, 'crossOrigin')}>Cross origin</Toggle>
         </div>
     );
 };
